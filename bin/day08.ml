a8db78c9ef0cb5cf65b771967a85b6e5a0a2058b
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2023-12-09 21:09:58 -0500

    Finished Day 8

--- bin/day08.ml
+++ bin/day08.ml
@@ -1,10 +1,10 @@
 open Core
 
-let final_destination = "ZZZ"
+let _final_destination = "ZZZ"
 
 type map_info = string * (string * string)
 
-let print_map_info (info : map_info) =
+let _print_map_info (info : map_info) =
   let dst, instr = info in
   let left, right = instr in
   let output = Printf.sprintf "DST: %s LEFT: %s RIGHT: %s" dst left right in
@@ -25,10 +25,6 @@ let parse_left_right_instruction instruction =
   let left = String.slice left 1 4 in
   let right = List.last_exn result in
   let right = String.slice right 1 4 in
-  (*
-     let output = Printf.sprintf "LEFT: %s RIGHT: %s" left right in
-     print_endline output;
-  *)
   left, right
 ;;
 
@@ -47,16 +43,13 @@ let parse_map map =
       let destination = split |> List.hd_exn |> String.strip in
       let left_right_instr = split |> List.last_exn |> String.strip in
       let left, right = parse_left_right_instruction left_right_instr in
-      (*
-         print_endline left_right_instr;
-      *)
       let element : map_info = destination, (left, right) in
       element :: acc)
   in
   List.rev result_map
 ;;
 
-let go_direction map (info : map_info) (curr_dir : char) =
+let go_direction map (info : map_info) (curr_dir : char) (part1 : bool) =
   let curr_dst, instr = info in
   let left, right = instr in
   let next_destination =
@@ -64,41 +57,37 @@ let go_direction map (info : map_info) (curr_dir : char) =
     then find_dst_in_map map left
     else find_dst_in_map map right
   in
-  let empty : map_info = "", ("", "") in
-  let out = "NEXT DESTINATION" in
-  print_endline "";
-  print_endline out;
-  print_map_info next_destination;
-  if String.equal curr_dst final_destination then empty, true else next_destination, false
+  let last_index = String.length curr_dst - 1 in
+  let last_ch = String.get curr_dst last_index in
+  if not part1
+  then if Char.equal last_ch 'Z' then empty, true else next_destination, false
+  else (
+    match curr_dst with
+    | "ZZZ" -> empty, true
+    | _ -> next_destination, false)
 ;;
 
-let rec _outer_iter
+let rec outer_iter
   map
   (info : map_info)
   (instr_list : char list)
   (copy_instr_list : char list)
   (past_result : map_info * bool)
   (steps : int)
+  (part_1 : bool)
   =
   match instr_list with
   | [] ->
     let _, cond = past_result in
     if cond
     then steps - 1
-    else _outer_iter map info copy_instr_list copy_instr_list past_result steps
+    else outer_iter map info copy_instr_list copy_instr_list past_result steps part_1
   | hd :: tl ->
-    (*
-       print_map_info info;
-    *)
-    let result = go_direction map info hd in
+    let result = go_direction map info hd part_1 in
     let info, cond = result in
-    (*
-       print_map_info info;
-    *)
-    steps |> Int.to_string |> print_endline;
     if cond
-    then _outer_iter map info [] copy_instr_list result (steps + 1)
-    else _outer_iter map info tl copy_instr_list result (steps + 1)
+    then outer_iter map info [] copy_instr_list result (steps + 1) part_1
+    else outer_iter map info tl copy_instr_list result (steps + 1) part_1
 ;;
 
 let find_starting_nodes map =
@@ -109,54 +98,44 @@ let find_starting_nodes map =
       let result = if Char.equal last_ch 'A' then element else empty in
       result :: acc)
   in
-  List.filter result ~f:(fun list -> not (is_empty list))
+  List.filter result ~f:(fun list -> not (is_empty list)) |> List.rev
 ;;
 
-let _all_reached_end nodes =
-  List.fold nodes ~init:true ~f:(fun acc item ->
-    let final_destination, _ = item in
-    let last_char = String.get final_destination (String.length final_destination - 1) in
-    let result = if Char.equal last_char 'Z' then true else false in
-    result && acc)
-;;
-
-let simulatenous_search starting_nodes map =
-  let next_nodes =
-    List.fold starting_nodes ~f:(fun acc start_node ->
-      let result = go_direction map start_node 'L' in
-      result :: acc)
-  in
-  print_endline "NEXT NODES: " in
-
-  ()
-;;
+let rec gcd x y = if y = 0 then x else gcd y (x mod y)
+let lcm x y = x / gcd x y * y
 
 let () =
-  let lines = Advent.read_file "inputs/day8/test3.txt" in
+  (* PART 1 *)
+  let lines = Advent.read_file "inputs/day8/input.txt" in
   let lines = lines |> List.filter ~f:(fun s -> not (String.is_empty s)) in
-  let _instructions = lines |> List.hd_exn |> String.to_list in
+  let instructions = lines |> List.hd_exn |> String.to_list in
   let map = List.tl_exn lines in
   let custom_map = parse_map map in
-  (*
-     List.iter custom_map ~f:(fun element ->
-     let dst, instr = element in
-     let left, right = instr in
-     let dst_fd, _ = find_dst_in_map custom_map right in
-     let output =
-     Printf.sprintf "DST: %s LEFT: %s RIGHT: %s FD: %s" dst left right dst_fd
-     in
-     print_endline output)
-  *)
-  (*
-     let _info = empty in
-     let start = find_dst_in_map custom_map "AAA" in
-     let steps = outer_iter custom_map start instructions instructions (empty, false) 0 in
-     steps |> Int.to_string |> print_endline
-  *)
+  let _info = empty in
+  let start = find_dst_in_map custom_map "AAA" in
+  let steps =
+    outer_iter custom_map start instructions instructions (empty, false) 0 true
+  in
+  let part1_answer = Printf.sprintf "PART 1 ANSWER: %d" steps in
+  print_endline part1_answer;
+  print_endline "";
+
+  (*PART 2 *)
   let starting_nodes = find_starting_nodes custom_map in
-  (*
-     List.iter starting_nodes ~f:print_map_info
-  *)
-  let _ = simulatenous_search starting_nodes custom_map in
-  ()
+  let steps_list =
+    List.fold starting_nodes ~init:[] ~f:(fun acc node ->
+      let steps =
+        outer_iter custom_map node instructions instructions (empty, false) 0 false
+      in
+      steps :: acc)
+  in
+  let answer =
+    List.fold steps_list ~init:1 ~f:(fun acc n ->
+      let result = lcm acc n in
+      result)
+  in
+  print_endline "";
+  let part2_answer = Printf.sprintf "PART 2 ANSWER: %d" answer in
+  print_endline part2_answer
 ;;
+
