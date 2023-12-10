8957f56179cc50c367a3150f2afd750ec54c397e
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2023-12-09 19:21:14 -0500

    Still working on day 8  part 2

--- bin/day08.ml
+++ bin/day08.ml
@@ -13,7 +13,7 @@ let print_map_info (info : map_info) =
 
 let empty : map_info = "", ("", "")
 
-let _is_empty (info : map_info) =
+let is_empty (info : map_info) =
   let dst, instr = info in
   let left, right = instr in
   String.equal dst "" && String.equal left "" && String.equal right ""
@@ -56,7 +56,7 @@ let parse_map map =
   List.rev result_map
 ;;
 
-let custom_iter map (info : map_info) (curr_dir : char) =
+let go_direction map (info : map_info) (curr_dir : char) =
   let curr_dst, instr = info in
   let left, right = instr in
   let next_destination =
@@ -72,7 +72,7 @@ let custom_iter map (info : map_info) (curr_dir : char) =
   if String.equal curr_dst final_destination then empty, true else next_destination, false
 ;;
 
-let rec outer_iter
+let rec _outer_iter
   map
   (info : map_info)
   (instr_list : char list)
@@ -85,26 +85,56 @@ let rec outer_iter
     let _, cond = past_result in
     if cond
     then steps - 1
-    else outer_iter map info copy_instr_list copy_instr_list past_result steps
+    else _outer_iter map info copy_instr_list copy_instr_list past_result steps
   | hd :: tl ->
     (*
        print_map_info info;
     *)
-    let result = custom_iter map info hd in
+    let result = go_direction map info hd in
     let info, cond = result in
     (*
        print_map_info info;
     *)
     steps |> Int.to_string |> print_endline;
     if cond
-    then outer_iter map info [] copy_instr_list result (steps + 1)
-    else outer_iter map info tl copy_instr_list result (steps + 1)
+    then _outer_iter map info [] copy_instr_list result (steps + 1)
+    else _outer_iter map info tl copy_instr_list result (steps + 1)
+;;
+
+let find_starting_nodes map =
+  let result =
+    List.fold map ~init:[] ~f:(fun acc element ->
+      let dst, _ = element in
+      let last_ch = String.get dst (String.length dst - 1) in
+      let result = if Char.equal last_ch 'A' then element else empty in
+      result :: acc)
+  in
+  List.filter result ~f:(fun list -> not (is_empty list))
+;;
+
+let _all_reached_end nodes =
+  List.fold nodes ~init:true ~f:(fun acc item ->
+    let final_destination, _ = item in
+    let last_char = String.get final_destination (String.length final_destination - 1) in
+    let result = if Char.equal last_char 'Z' then true else false in
+    result && acc)
+;;
+
+let simulatenous_search starting_nodes map =
+  let next_nodes =
+    List.fold starting_nodes ~f:(fun acc start_node ->
+      let result = go_direction map start_node 'L' in
+      result :: acc)
+  in
+  print_endline "NEXT NODES: " in
+
+  ()
 ;;
 
 let () =
-  let lines = Advent.read_file "inputs/day8/input.txt" in
+  let lines = Advent.read_file "inputs/day8/test3.txt" in
   let lines = lines |> List.filter ~f:(fun s -> not (String.is_empty s)) in
-  let instructions = lines |> List.hd_exn |> String.to_list in
+  let _instructions = lines |> List.hd_exn |> String.to_list in
   let map = List.tl_exn lines in
   let custom_map = parse_map map in
   (*
@@ -117,8 +147,16 @@ let () =
      in
      print_endline output)
   *)
-  let _info = empty in
-  let start = find_dst_in_map custom_map "AAA" in
-  let steps = outer_iter custom_map start instructions instructions (empty, false) 0 in
-  steps |> Int.to_string |> print_endline
+  (*
+     let _info = empty in
+     let start = find_dst_in_map custom_map "AAA" in
+     let steps = outer_iter custom_map start instructions instructions (empty, false) 0 in
+     steps |> Int.to_string |> print_endline
+  *)
+  let starting_nodes = find_starting_nodes custom_map in
+  (*
+     List.iter starting_nodes ~f:print_map_info
+  *)
+  let _ = simulatenous_search starting_nodes custom_map in
+  ()
 ;;
